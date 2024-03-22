d486f9d98abe492013378a577a7bbff67bd82dda
MekNCheeze <yeetthechild420@gmail.com>
2024-03-22 11:36:20 +0100

    Feinarbeit

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function changeTea(){
         var tea = String(tea).toLowerCase();
         const mytea = tea.replace(/\s/g, '');
         const image = document.getElementById("teapot");
-        if (mytea == 'black tea' || mytea == 'black'){
+        if (mytea == 'blacktea' || mytea == 'black'){
             image.src = "images/Black.png";
         }
-        else if (mytea == 'jasmine tea' ||mytea == 'jasmine'){
+        else if (mytea == 'jasminetea' ||mytea == 'jasmine'){
             image.src = "images/Jasmine.png";
         }
-        else if (mytea == 'cherry-blossom tea' || mytea == 'cherry-blossom'){
+        else if (mytea == 'cherry-blossomtea' || mytea == 'cherry-blossom'){
             image.src = "images/Cherry_Blossom.png";
         }
-        else if (mytea == 'purple tea' || mytea == 'purple'){
+        else if (mytea == 'purpletea' || mytea == 'purple'){
             image.src = "images/Purple.png";
         }
         else{
